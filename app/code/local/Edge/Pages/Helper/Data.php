144664526730c4c1162cbbaa07f4078ddda6da66
Bruno Dias <bruno@outeredgeuk.com>
2015-05-21 13:40:50 +0100

    Adding helper function get cms page by type name

--- app/code/local/Edge/Pages/Helper/Data.php
+++ app/code/local/Edge/Pages/Helper/Data.php
@@ -2,5 +2,22 @@
 
 class Edge_Pages_Helper_Data extends Mage_Core_Helper_Abstract
 {
-    
+    public function getPageByType($name)
+    {
+        $pageType = Mage::getModel('pages/page_type')->load($name, 'name');
+
+        if (empty($pageType->getData())) {
+            return "Page Type don't exist.";
+        }
+
+        $result = Mage::getModel('cms/page')
+            ->getCollection()
+            ->addFieldToFilter('page_type', array('eq' => $pageType->getId()));
+
+        if (empty($result->getData())) {
+            return "No cms page with that type.";
+        }
+
+        return $result;
+    }
 }
